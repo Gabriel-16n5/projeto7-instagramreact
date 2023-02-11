0411e097b70d5e10468f08b8a1c34fb241585660
Gabriel-16n5 <gabrielgranjeia@alunos.utfpr.edu.br>
2023-02-11 20:32:18 -0300

    Ajustes na modularização

---
+++ projeto7-instagramreact/src/data/infoPost.js
@@ -0,0 +1,23 @@
+const infoPost = [
+    {
+        urlUser: "assets/img/barked.svg",
+        user: "barked",
+        urlContent: "assets/img/dog.svg",
+        altContent: "dog",
+        urlLike: "assets/img/adorable_animals.svg",
+        altLike: "adorable_animals",
+        texto: "Curtido por <strong>adorable_animals</strong> e <strong>outras 99.159 pessoas</strong>"
+    },
+    {
+        urlUser: "assets/img/meowed.svg",
+        user: "meowed",
+        urlContent: "assets/img/gato-telefone.svg",
+        altContent: "gato-telefone",
+        urlLike: "assets/img/respondeai.svg",
+        altLike: "respondeai",
+        texto: "Curtido por <strong>respondeai</strong> e <strong>outras 101.523 pessoas</strong>"
+    },
+   
+];
+
+export default infoPost;
