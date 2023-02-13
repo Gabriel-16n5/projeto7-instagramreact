b719cf7f183e063ae748a50961330c194b03f229
Gabriel-16n5 <gabrielgranjeia@alunos.utfpr.edu.br>
2023-02-12 21:37:58 -0300

    desisto

---
+++ src/components/Stories.jsx
@@ -0,0 +1,15 @@
+import Story from "./Story";
+
+function Stories(){
+    return(
+        <div class="stories">
+        <Story />
+        <div class="setinha">
+          <ion-icon name="chevron-forward-circle"></ion-icon>
+        </div>
+      </div>
+
+    )
+}
+
+export default Stories;
---
+++ src/data/infoSugestoes.js
@@ -0,0 +1,34 @@
+const infoSugestoes = [
+    {
+        url: "assets/img/bad.vibes.memes.svg",
+        urlAlt: "bad.vibes.memes.svg",
+        user: "bad.vibes.memes",
+        stats: "Segue você"
+    },
+    {
+        url: "assets/img/chibirdart.svg",
+        urlAlt: "chibirdart",
+        user: "chibirdart",
+        stats: "Segue você"
+    },
+    {
+        url: "assets/img/razoesparaacreditar.svg",
+        urlAlt: "razoesparaacreditar",
+        user: "razoesparaacreditar",
+        stats: "Novo no Instagram"
+    },
+    {
+        url: "assets/img/adorable_animals.svg",
+        urlAlt: "adorable_animals",
+        user: "adorable_animals",
+        stats: "Segue você"
+    },
+    {
+        url: "assets/img/smallcutecats.svg",
+        urlAlt: "smallcutecats",
+        user: "smallcutecats",
+        stats: "Segue você"
+    },
+];
+
+export default infoSugestoes;
