17bfa6f5c78530cbee40c16d2df9910e4cad2588
Gabriel-16n5 <gabrielgranjeia@alunos.utfpr.edu.br>
2023-02-12 22:18:36 -0300

    aaaaa

---
+++ projeto7-instagramreact/src/components/Usuario.jsx
@@ -0,0 +1,14 @@
+const usuario = [
+    {
+        p1:467,
+        p2:894,
+        p3:653
+    },
+    {
+        u1:"adorable_animals",
+        u2:"respondeai",
+        u3:"AmoAmar"
+    },
+]
+
+export default usuario;
---
+++ projeto7-instagramreact/src/data/infoStory.js
@@ -0,0 +1,37 @@
+const infoStory = [
+    {
+        url: "assets/img/9gag.svg",
+        user: "9gag"
+    },
+    {
+        url: "assets/img/meowed.svg",
+        user: "meowed"
+    },
+    {
+        url: "assets/img/barked.svg",
+        user: "barked"
+    },
+    {
+        url: "assets/img/nathanwpylestrangeplanet.svg",
+        user: "nathanwpylestrangeplanet"
+    },
+    {
+        url: "assets/img/wawawicomics.svg",
+        user: "wawawicomics"
+    },
+    {
+        url: "assets/img/respondeai.svg",
+        user: "respondeai"
+    },
+    {
+        url: "assets/img/filomoderna.svg",
+        user: "filomoderna"
+    },
+    {
+        url: "assets/img/memeriagourmet.svg",
+        user: "memeriagourmet"
+    },
+    
+];
+
+export default infoStory;
